f03f6755b0113f7e87af73b42e2b6606ba21445a
Jayden <dshung1997@gmail.com>
2021-04-19 23:12:11 +0700

    wip: implement AutomaticColorization

--- client/src/components/AutomaticColorization/index.js
+++ client/src/components/AutomaticColorization/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import classNames from "classnames";
+
+import PhotoUploader from "components/shared/PhotoUploader";
 
 const AutomaticColorization = () => {
-  return <div>AutomaticColorization</div>;
+  return (
+    <div className={classNames("u-widthFull u-heightFull", "u-flex u-flexColumn")}>
+      <div>
+        <PhotoUploader />
+      </div>
+    </div>
+  );
 };
 
 export default AutomaticColorization;
---
+++ client/src/components/shared/PhotoUploader.js
@@ -0,0 +1,69 @@
+import React from "react";
+import classNames from "classnames";
+
+import { Button, Form } from "@ahaui/react";
+
+const PhotoUploader = () => {
+  const dragging = false;
+  const invalid = false;
+  return (
+    <div
+      className={classNames(
+        "u-roundedMedium u-border u-borderDashed u-borderSmall u-paddingMedium u-flex u-justifyContentCenter",
+        !dragging && "u-backgroundWhite",
+        dragging && !invalid && "u-borderPrimary u-backgroundPrimaryLight",
+        dragging && invalid && "u-borderNegative u-backgroundNegativeLight"
+      )}
+    >
+      <div className="u-flex u-flexColumn">
+        {true && (
+          <div className="u-marginBottomMedium u-flex">
+            <div className="u-positionRelative u-marginRightSmall u-flexShrink-0">
+              <img src={require("assets/drop-zone-upload.svg")} className="u-maxWidthFull" alt="" />
+            </div>
+            <div className="u-textGray u-text200" data-name="file-name">
+              Drop your file here (.csv) or browse a file
+            </div>
+          </div>
+        )}
+        {true && (
+          <div className="u-marginBottomExtraSmall">
+            <div className="u-positionRelative u-marginRightSmall u-flexShrink-0 u-textCenter">
+              <img src={require("assets/drop-zone-upload.svg")} className="u-maxWidthFull" alt="" />
+            </div>
+            <div className="u-textGray" data-name="file-name">
+              <span className="u-textWordBreakAll u-text200">File Name</span>
+            </div>
+          </div>
+        )}
+        <div className="u-textCenter">
+          <Form.Group className="u-marginNone">
+            <input
+              className="u-hidden"
+              type="file"
+              id="submissionFile"
+              onChange={() => {}}
+              accept={".csv"}
+              disabled={false}
+            />
+            {true && (
+              // eslint-disable-next-line jsx-a11y/anchor-is-valid
+              <a href="#" className="hover:u-textDecorationNone u-block">
+                <label htmlFor="submissionFile" className="u-text100 u-cursorPointer">
+                  Change file
+                </label>
+              </a>
+            )}
+            {true && (
+              <Button as="label" htmlFor="submissionFile" variant="secondary">
+                BROWSE FILE...
+              </Button>
+            )}
+          </Form.Group>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhotoUploader;
