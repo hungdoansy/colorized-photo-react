e9c332cd72283a4ea95fa36eaa4ea46d8f9b08fa
Jayden <dshung1997@gmail.com>
2021-04-23 17:56:55 +0700

    refactor(server): use js

---
+++ server/src/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+
+const app = express();
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(fileUpload());
+
+// Upload Endpoint
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
+
+  console.log(req.files);
+
+  const file = req.files.file;
+
+  file.mv(`../public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+app.listen(9001, () => console.log("Server Started..."));
