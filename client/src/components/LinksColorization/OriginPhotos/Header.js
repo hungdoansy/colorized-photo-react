dd6f9b08e8a1cf86eed6739bc423d1517e765808
Jayden <dshung1997@gmail.com>
2021-05-26 14:51:36 +0700

    feat(client/links-colorization): enable selecting an image from links to be colorized using guider

--- client/src/components/LinksColorization/OriginPhotos/Header.js
+++ client/src/components/LinksColorization/OriginPhotos/Header.js
@@ -11,6 +11,7 @@ import linksColorizationStore, {
   getSelectedPhotos,
   updateColorizedPhotos,
 } from "components/LinksColorization/store";
+import { removePhoto, setPhoto } from "components/GuidedColorization/store";
 import config from "envConfig";
 import { CustomButtonCSS } from "utils/style";
 
@@ -48,6 +49,14 @@ const Header = ({ className }) => {
       });
   };
 
+  const handleClickColorizeWithGuider = () => {
+    const photoId = linksColorizationState.ids.find((id) => linksColorizationState.photoById[id].selected);
+    const photo = linksColorizationState.photoById[photoId];
+
+    removePhoto();
+    setPhoto(photo.originUrl, "guided");
+  };
+
   return (
     <div
       className={classNames(className, "u-widthFull sticky-header u-paddingVerticalExtraSmall u-paddingTopMedium")}
@@ -57,47 +66,60 @@ const Header = ({ className }) => {
       }}
     >
       <p className="u-widthFull u-textWhite u-text600 u-fontMedium u-marginBottomExtraSmall">Origin photos</p>
-      <div
-        className="u-flex"
-        style={{
-          columnGap: 8,
-        }}
-      >
-        <Button
-          size="small"
-          variant="primary_outline"
-          onClick={selectAllPhoto}
-          disabled={loading}
-          className="custom-button u-text300"
+
+      <div className="u-flex u-justifyContentBetween u-alignItemsCenter">
+        <div
+          className="u-flex"
+          style={{
+            columnGap: 8,
+          }}
         >
-          Select All
-        </Button>
+          <Button
+            size="small"
+            variant="primary_outline"
+            onClick={selectAllPhoto}
+            disabled={loading}
+            className="custom-button u-text300"
+          >
+            Select All
+          </Button>
+
+          <Button
+            size="small"
+            variant="primary_outline"
+            onClick={unselectAllPhoto}
+            disabled={loading}
+            className="custom-button u-text300"
+          >
+            Unselect All
+          </Button>
+
+          <Button
+            size="small"
+            variant="primary_outline"
+            onClick={handleColorizeSelectedPhotos}
+            disabled={loading}
+            className="custom-button u-text300"
+          >
+            Colorize Selected (<span>{numberOfSelectedPhotos}</span>)
+          </Button>
+
+          {loading && (
+            <div className="u-flex u-justifyContentCenter u-alignItemsCenter">
+              <Loader size="small" className="u-textNeutral50 u-marginHorizontalExtraSmall" />;
+            </div>
+          )}
+        </div>
 
         <Button
           size="small"
           variant="primary_outline"
-          onClick={unselectAllPhoto}
-          disabled={loading}
+          onClick={handleClickColorizeWithGuider}
+          disabled={numberOfSelectedPhotos !== 1}
           className="custom-button u-text300"
         >
-          Unselect All
+          Colorize with guider
         </Button>
-
-        <Button
-          size="small"
-          variant="primary_outline"
-          onClick={handleColorizeSelectedPhotos}
-          disabled={loading}
-          className="custom-button u-text300"
-        >
-          Colorize Selected (<span>{numberOfSelectedPhotos}</span>)
-        </Button>
-
-        {loading && (
-          <div className="u-flex u-justifyContentCenter u-alignItemsCenter">
-            <Loader size="small" className="u-textNeutral50 u-marginHorizontalExtraSmall" />;
-          </div>
-        )}
       </div>
     </div>
   );
